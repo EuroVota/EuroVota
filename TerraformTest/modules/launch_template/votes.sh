fe7ba53652f6202f6f52382de8e74d7e40c4e844
deeek8 <100050276+deeek8@users.noreply.github.com>
2024-07-23 18:23:40 +0200

    cambios en launch templates

--- TerraformTest/modules/launch_template/votes.sh
+++ TerraformTest/modules/launch_template/votes.sh
@@ -3,10 +3,10 @@ set -x
 
 sudo chmod 777 /var/run/docker.sock
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 259683775580.dkr.ecr.us-east-1.amazonaws.com/eurovota
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 259683775580.dkr.ecr.us-east-1.amazonaws.com/votes
 
 sudo systemctl start docker
 
-docker pull 259683775580.dkr.ecr.us-east-1.amazonaws.com/eurovota:users
+docker pull 259683775580.dkr.ecr.us-east-1.amazonaws.com/votes:latest
 
-sudo docker run -d -p 9002:9002 --restart unless-stopped --name votes 259683775580.dkr.ecr.us-east-1.amazonaws.com/eurovota/votes:latest
+sudo docker run -d -p 9002:9002 --restart unless-stopped 259683775580.dkr.ecr.us-east-1.amazonaws.com/votes:latest
